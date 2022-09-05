1dc2afcb3823264e45547d38ed5a66a61caca9cf
Deniz <dennos746@gmail.com>
2022-09-05 21:21:29 +0300

    Proxy Use Preference

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def clear():
     system('cls')
 
 clear()
-proxies = open('proxies.txt','r',encoding='utf-8').read().splitlines()
+
 
 count = 0
 
@@ -14,16 +14,10 @@ print(f"{Fore.LIGHTYELLOW_EX}[?] Github View Counter Link: ") #Example View Coun
 counter_url = input('> ')
 print(f"{Fore.LIGHTYELLOW_EX}\n[?] Number of Threads: ")
 threads_number = int(input('> '))
-
+print(f"{Fore.LIGHTYELLOW_EX}\n[?] Use a Proxy: (y/n) ")
+proxy_use = input('> ')
 def run():
     while True:
-        proxy = choice(proxies)
-
-        proxy_dict = {
-            "http": f"http://{proxy}",
-            "https": f"https://{proxy}"
-        }
-
         headers = {
             "authority": "camo.githubusercontent.com",
             "method": "GET",
@@ -38,23 +32,49 @@ def run():
             "upgrade-insecure-requests": "1",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
         }
-        try:
-            url = counter_url
-            global r
-            r = requests.get(url=url,headers=headers,proxies=proxy_dict)
-        except requests.exceptions.ProxyError:
-            print(f"{Fore.LIGHTRED_EX}[-] Bad Proxy: {proxy}")
-        except requests.exceptions.MissingSchema:
-            print(f"{Fore.LIGHTRED_EX}[-] Invalid link! Please make sure you enter the correct link.")
-            input('')
-            exit()
-        if r.status_code == 200:
-            global count
-            count += 1
+        if proxy_use == 'y':
+            proxies = open('proxies.txt','r',encoding='utf-8').read().splitlines()
+            proxy = choice(proxies)
 
-            print(f"{Fore.LIGHTGREEN_EX}[+] Successful Request! Total Successful Requests Sent: {count}")
-        else:
-            print(f"{Fore.LIGHTRED_EX}[-] Error Requests.")
+            proxy_dict = {
+                "http": f"http://{proxy}",
+                "https": f"https://{proxy}"
+            }
+
+            try:
+                url = counter_url
+                global r
+                r = requests.get(url=url,headers=headers,proxies=proxy_dict)
+            except requests.exceptions.ProxyError:
+                print(f"{Fore.LIGHTRED_EX}[-] Bad Proxy: {proxy}")
+            except requests.exceptions.MissingSchema:
+                print(f"{Fore.LIGHTRED_EX}[-] Invalid link! Please make sure you enter the correct link.")
+                input('')
+                exit()
+            if r.status_code == 200:
+                global count
+                count += 1
+
+                print(f"{Fore.LIGHTGREEN_EX}[+] Successful Request! Total Successful Requests Sent: {count}")
+            else:
+                print(f"{Fore.LIGHTRED_EX}[-] Error Requests.")
+        elif proxy_use == 'n':
+            try:
+                url = counter_url
+                global req
+                req = requests.get(url=url,headers=headers)
+            except requests.exceptions.ProxyError:
+                print(f"{Fore.LIGHTRED_EX}[-] Bad Proxy: {proxy}")
+            except requests.exceptions.MissingSchema:
+                print(f"{Fore.LIGHTRED_EX}[-] Invalid link! Please make sure you enter the correct link.")
+                input('')
+                exit()
+            if req.status_code == 200:
+                count += 1
+
+                print(f"{Fore.LIGHTGREEN_EX}[+] Successful Request! Total Successful Requests Sent: {count}")
+            else:
+                print(f"{Fore.LIGHTRED_EX}[-] Error Requests.")
 
 
 for i in range(1,threads_number+1):
