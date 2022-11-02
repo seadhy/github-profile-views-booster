753059936b5280f45420b97db88e55801160e4bb
Deniz <dennos746@gmail.com>
2022-11-02 23:46:55 +0300

    Proxies errors fixed!

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def clear():
 def safe_print(*args):
     lock.acquire()
     for arg in args: print(arg, end=' ')
+    print()
     lock.release()
 
 def run():
@@ -49,44 +50,64 @@ def run():
             proxy = choice(proxies)
 
             proxy_dict = {
-                "http": f"http://{proxy}",
-                "https": f"http://{proxy}"
+                "http://": f"http://{proxy}",
+                "https://": f"http://{proxy}"
             }
 
             try:
                 url = counter_url
                 global r
-                r = httpx.get(url=url, headers=headers, proxies=proxy_dict)
+                r = httpx.get(url=url, headers=headers, proxies=proxy_dict, timeout=10)
             except httpx.ProxyError:
                 safe_print(f"{Fore.LIGHTRED_EX}[-] Bad proxy: {proxy}")
+                proxies.remove(proxy)
+                continue
+            except httpx.ConnectError:
+                safe_print(f"{Fore.LIGHTRED_EX}[-] Connect Error proxy: {proxy}")
+                continue
+            except httpx.RemoteProtocolError:
+                safe_print(f"{Fore.LIGHTRED_EX}[-] Remote Protocol Error, Retrying...")
+                continue
+            except httpx.ConnectTimeout:
+                safe_print(f"{Fore.LIGHTRED_EX}[-] Connect Timeout proxy: {proxy}")
+                continue
+            except httpx.ReadTimeout:
+                safe_print(f"{Fore.LIGHTRED_EX}[-] Read Timeout proxy: {proxy}")
+                continue
             except ValueError:
                 safe_print(f"{Fore.LIGHTRED_EX}[-] Please change the 'counter_url' with a valid camo url in 'config.json'")
-                print('')
                 break
             if r.status_code == 200:
                 global count
                 count += 1
-
                 safe_print(f"{Fore.LIGHTGREEN_EX}[+] Successful request! Total successful requests sent: {count}")
+                
+            elif 'Bad Signature' in r.text:
+                safe_print(f"{Fore.LIGHTRED_EX}[-] Invalid Camo Link! Please change valid link. Example Camo Link: https://camo.githubusercontent.com/4d29071feb1358f324bd018ad789e974f4c5963e91aa6bbc57dec9bb118a67c9/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d736561646879")
+                break
             else:
                 safe_print(f"{Fore.LIGHTRED_EX}[-] Error request.")
+
         elif use_proxy == 'n' or use_proxy == 'no':
             try:
                 url = counter_url
                 global req
-                req = httpx.get(url=url,headers=headers)
-            except httpx.ProxyError:
-                safe_print(f"{Fore.LIGHTRED_EX}[-] Bad Proxy: {proxy}")
+                req = httpx.get(url=url,headers=headers,timeout=10)
+                
+            except httpx.ConnectTimeout:
+                safe_print(f"{Fore.LIGHTRED_EX}[-] Timeout error.")
+                continue
+
             if req.status_code == 200:
                 count += 1
 
                 safe_print(f"{Fore.LIGHTGREEN_EX}[+] Successful request! Total successful requests sent: {count}")
-                print()
+                
             elif 'Bad Signature' in req.text:
                 safe_print(f"{Fore.LIGHTRED_EX}[-] Invalid Camo Link! Please change valid link. Example Camo Link: https://camo.githubusercontent.com/4d29071feb1358f324bd018ad789e974f4c5963e91aa6bbc57dec9bb118a67c9/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d736561646879")
                 break
             else:
-                safe_print(req.text)
+                #safe_print(req.text)
                 safe_print(f"{Fore.LIGHTRED_EX}[-] Error request.")
 
 # * Run Booster * #
